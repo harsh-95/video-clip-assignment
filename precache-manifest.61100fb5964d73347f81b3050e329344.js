6b34c0ed6ac07442c123229099ec4344c6c9322c
Harsh Gupta <harshgupta951114@gmail.com>
2020-06-18 22:11:07 +0530

    Updates

---
+++ precache-manifest.61100fb5964d73347f81b3050e329344.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e09085c54637a95c37ba4c876e6fd0c7",
+    "url": "/video-clip-assignment/index.html"
+  },
+  {
+    "revision": "edb4ac9d467c46651300",
+    "url": "/video-clip-assignment/static/css/main.ba9cfb0f.chunk.css"
+  },
+  {
+    "revision": "d7ae52db6fa813477f48",
+    "url": "/video-clip-assignment/static/js/2.68899435.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/video-clip-assignment/static/js/2.68899435.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edb4ac9d467c46651300",
+    "url": "/video-clip-assignment/static/js/main.895076ff.chunk.js"
+  },
+  {
+    "revision": "647d1f42b5a66696e9d5",
+    "url": "/video-clip-assignment/static/js/runtime-main.536575ed.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/video-clip-assignment/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d422bedd051bc0001121e61779f6e049",
+    "url": "/video-clip-assignment/static/media/samplevideo.d422bedd.mp4"
+  }
+]);
